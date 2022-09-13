45396c841f2cb7308a65b77fc930b90c9ea63119
JSantangelo83 <joakinsant@gmail.com>
2022-09-13 13:20:52 -0300

    Added lightdm, pacman.conf, themes, and screenlayout

---
+++ .screenlayout/dual-screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1360x768 --pos 1366x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --primary --mode 1366x768 --pos 0x0 --rotate normal
