dec1cf070913e1d46e42e00bbb47f8d119dfee71
JSantangelo83 <joakinsant@gmail.com>
2023-08-28 02:19:26 -0300

    multiples fixes

--- zsh/perm_variables.sh
+++ zsh/perm_variables.sh
@@ -71,5 +71,5 @@ function upvar(){
 }
 
 ### Actual Variables: 
-export tip='10.129.101.175'
-export starting_path='/home/js/hacking/hackthebox/machines/cybermonday/content'
+export tip='10.129.102.66'
+export starting_path='/home/js/hacking/hackthebox/machines/zipping/recon'
