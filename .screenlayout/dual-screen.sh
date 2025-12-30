4d1941573b127d9cde28dc8b6b0f2a0a1f8e61f9
JSantangelo83 <joakinsant@gmail.com>
2025-12-30 02:40:11 -0300

    Added lightdm

--- .screenlayout/dual-screen.sh
+++ .screenlayout/dual-screen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --mode 1360x768 --pos 3328x190 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --primary --mode 2560x1440 --pos 768x0 --rotate normal --output HDMI-A-0 --mode 1366x768 --pos 0x0 --rotate right
+xrandr --output DisplayPort-0 --mode 1440x900 --pos 3460x213 --rate 74.98 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --primary --rate 164.83 --mode 2560x1440 --pos 900x0 --rotate normal --output HDMI-A-0 --mode 1440x900 --rate 74.98 --pos 0x0 --rotate left
