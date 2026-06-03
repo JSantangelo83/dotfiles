0acddb081cb0312349c508e893c8aff2a0af1bbf
JSantangelo83 <joakinsant@gmail.com>
2026-06-02 21:41:15 -0300

    Removed X11 leftovers

--- .screenlayout/layout.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DisplayPort-0 --mode 1440x900 --pos 3460x2160 --rotate normal --output DisplayPort-1 --mode 3840x2160 --pos 393x0 --rotate normal --output DisplayPort-2 --primary --mode 2560x1440 --pos 900x2160 --rotate normal --output HDMI-A-0 --mode 1440x900 --pos 0x2160 --rotate left
